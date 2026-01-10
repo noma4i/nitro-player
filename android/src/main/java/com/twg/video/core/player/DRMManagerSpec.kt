3cf736546430b0ba565c40a4a12d56d014cc1a42
Alexander Tsirel <noma4i@gmail.com>
2026-01-10 14:15:00 +0300

    Add android player core

---
+++ android/src/main/java/com/twg/video/core/player/DRMManagerSpec.kt
@@ -0,0 +1,12 @@
+package com.twg.video.core.player
+
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.drm.DrmSessionManager
+import com.margelo.nitro.video.NativeDrmParams
+
+@UnstableApi
+interface DRMManagerSpec {
+  fun getDRMConfiguration(drmParams: NativeDrmParams): MediaItem.DrmConfiguration
+  fun buildDrmSessionManager(drmParams: NativeDrmParams): DrmSessionManager
+}
