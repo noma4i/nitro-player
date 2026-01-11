21fc4cef7418290e1baf79c8ce089a3a981e74d0
Alexander Tsirel <noma4i@gmail.com>
2026-01-11 10:30:00 +0300

    Add android hybrid classes

---
+++ android/src/main/java/com/twg/video/hybrids/videoplayersource/HybridVideoPlayerSource.kt
@@ -0,0 +1,57 @@
+package com.margelo.nitro.video
+
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.drm.DrmSessionManager
+import androidx.media3.exoplayer.source.MediaSource
+import com.margelo.nitro.NitroModules
+import com.margelo.nitro.core.Promise
+import com.twg.video.core.LibraryError
+import com.twg.video.core.player.DRMManagerSpec
+import com.twg.video.core.player.buildMediaSource
+import com.twg.video.core.player.createMediaItemFromVideoConfig
+import com.twg.video.core.utils.SourceLoader
+import com.twg.video.core.utils.VideoInformationUtils
+
+class HybridVideoPlayerSource(): HybridVideoPlayerSourceSpec() {
+  override lateinit var uri: String
+  override lateinit var config: NativeVideoConfig
+
+  private lateinit var mediaItem: MediaItem
+  lateinit var mediaSource: MediaSource
+
+  var drmManager: DRMManagerSpec? = null
+
+  @UnstableApi
+  var drmSessionManager: DrmSessionManager? = null
+
+  internal val sourceLoader = SourceLoader()
+
+  constructor(config: NativeVideoConfig) : this() {
+    this.uri = config.uri
+    this.config = config
+
+    this.mediaItem = createMediaItemFromVideoConfig(this)
+
+    NitroModules.applicationContext?.let {
+      this.mediaSource = buildMediaSource(
+        context = it,
+        source = this,
+        mediaItem
+      )
+    } ?: run {
+      throw LibraryError.ApplicationContextNotFound
+    }
+  }
+
+  override fun getAssetInformationAsync(): Promise<VideoInformation> {
+    return Promise.async {
+      return@async sourceLoader.load {
+        VideoInformationUtils.fromUri(uri, config.headers ?: emptyMap())
+      }
+    }
+  }
+
+  override val memorySize: Long
+    get() = 0
+}
