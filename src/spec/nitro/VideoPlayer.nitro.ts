c201b31b300226f55a84cce2c3117a7d433c4548
Alexander Tsirel <noma4i@gmail.com>
2026-03-11 10:15:00 +0300

    Strip PiP, DRM, subtitles

--- src/spec/nitro/VideoPlayer.nitro.ts
+++ src/spec/nitro/VideoPlayer.nitro.ts
@@ -1,7 +1,6 @@
 import type { HybridObject } from 'react-native-nitro-modules';
 import type { MemorySnapshot } from '../../core/types/MemorySnapshot';
 import type { PlaybackState } from '../../core/types/PlaybackState';
-import type { TextTrack } from '../../core/types/TextTrack';
 import type { VideoPlayerBase } from '../../core/types/VideoPlayerBase';
 import type { VideoPlayerEventEmitter } from './VideoPlayerEventEmitter.nitro';
 import type { VideoPlayerSource } from './VideoPlayerSource.nitro';
@@ -9,55 +8,12 @@ import type { VideoPlayerSource } from './VideoPlayerSource.nitro';
 export interface VideoPlayer
   extends HybridObject<{ ios: 'swift'; android: 'kotlin' }>,
     VideoPlayerBase {
-  // Override with (hybrid) VideoPlayerSource
   readonly source: VideoPlayerSource;
-
-  // Holder of the video player events.
   readonly eventEmitter: VideoPlayerEventEmitter;
-
-  /**
-   * Full playback snapshot exposed synchronously to JS.
-   */
   readonly playbackState: PlaybackState;
-
-  /**
-   * Native memory footprint snapshot for player + source.
-   */
   readonly memorySnapshot: MemorySnapshot;
 
-  /**
-   * Show playback controls in the notifications area
-   *
-   * @note on Android, this can be overridden by {@linkcode VideoPlayer.playInBackground}, as Android requires
-   * a foreground service to show notifications while the app is in the background.
-   *
-   * @default false
-   */
-  showNotificationControls: boolean;
-
-  /**
-   * Replace the current source of the player.
-   * @param source - The new source of the video.
-   * @note If you want to clear the source, you can pass null. It has the same effect as {@link release}.
-   * see {@link VideoPlayerSourceBase}
-   */
   replaceSourceAsync(source: VideoPlayerSource | null): Promise<void>;
-
-  /**
-   * Get all available text tracks for the current source.
-   * @returns Array of available text tracks
-   */
-  getAvailableTextTracks(): TextTrack[];
-
-  /**
-   * Select a text track to display.
-   * @param textTrack - Text track to select, or null to unselect current track
-   */
-  selectTextTrack(textTrack: TextTrack | null): void;
-
-  /**
-   * Releases the player's native resources and releases native state.
-   */
   release(): void;
 }
 
--- src/spec/nitro/VideoPlayerEventEmitter.nitro.ts
+++ src/spec/nitro/VideoPlayerEventEmitter.nitro.ts
@@ -4,10 +4,8 @@ import type {
   onLoadData,
   onLoadStartData,
   onVolumeChangeData,
-  TimedMetadata,
 } from '../../core/types/Events';
 import type { PlaybackState } from '../../core/types/PlaybackState';
-import type { TextTrack } from '../../core/types/TextTrack';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { VideoPlayerEvents } from '../../core/types/Events';
@@ -18,124 +16,23 @@ export interface ListenerSubscription {
 
 export interface VideoPlayerEventEmitter
   extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
-  /**
-   * Adds a listener for the `onAudioBecomingNoisy` event.
-   * @see {@link VideoPlayerEvents.onAudioBecomingNoisy}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnAudioBecomingNoisyListener(listener: () => void): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onAudioFocusChange` event.
-   * @see {@link VideoPlayerEvents.onAudioFocusChange}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnAudioFocusChangeListener(
-    listener: (hasAudioFocus: boolean) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onBandwidthUpdate` event.
-   * @see {@link VideoPlayerEvents.onBandwidthUpdate}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
   addOnBandwidthUpdateListener(
     listener: (data: BandwidthData) => void
   ): ListenerSubscription;
 
-  /**
-   * Adds a listener for the `onControlsVisibleChange` event.
-   * @see {@link VideoPlayerEvents.onControlsVisibleChange}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnControlsVisibleChangeListener(
-    listener: (visible: boolean) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onExternalPlaybackChange` event.
-   * @see {@link VideoPlayerEvents.onExternalPlaybackChange}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnExternalPlaybackChangeListener(
-    listener: (externalPlaybackActive: boolean) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onLoad` event.
-   * @see {@link VideoPlayerEvents.onLoad}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
   addOnLoadListener(listener: (data: onLoadData) => void): ListenerSubscription;
 
-  /**
-   * Adds a listener for the `onLoadStart` event.
-   * @see {@link VideoPlayerEvents.onLoadStart}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
   addOnLoadStartListener(
     listener: (data: onLoadStartData) => void
   ): ListenerSubscription;
 
-  /**
-   * Adds a listener for the `onPlaybackState` event.
-   * @see {@link VideoPlayerEvents.onPlaybackState}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
   addOnPlaybackStateListener(
     listener: (state: PlaybackState) => void
   ): ListenerSubscription;
 
-  /**
-   * Adds a listener for the `onTimedMetadata` event.
-   * @see {@link VideoPlayerEvents.onTimedMetadata}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnTimedMetadataListener(
-    listener: (data: TimedMetadata) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onTextTrackDataChanged` event.
-   * @see {@link VideoPlayerEvents.onTextTrackDataChanged}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnTextTrackDataChangedListener(
-    listener: (data: string[]) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onTrackChange` event.
-   * @see {@link VideoPlayerEvents.onTrackChange}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
-  addOnTrackChangeListener(
-    listener: (track: TextTrack | null) => void
-  ): ListenerSubscription;
-
-  /**
-   * Adds a listener for the `onVolumeChange` event.
-   * @see {@link VideoPlayerEvents.onVolumeChange}
-   * @param listener - The listener to add.
-   * @returns A subscription object that can be used to remove the listener.
-   */
   addOnVolumeChangeListener(
     listener: (data: onVolumeChangeData) => void
   ): ListenerSubscription;
 
-  /**
-   * Clears all listeners from the event emitter.
-   */
   clearAllListeners(): void;
 }
