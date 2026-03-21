4f264763580ec258af5891d3c93d6af4bccb020c
Alexander Tsirel <noma4i@gmail.com>
2026-03-22 02:44:55 +1100

    Emit playback state on play/pause

--- src/core/hooks/usePlaybackState.ts
+++ src/core/hooks/usePlaybackState.ts
@@ -75,6 +75,7 @@ export const usePlaybackState = (player: NitroPlayer | null | undefined, options
     }
 
     const subscription = player.addEventListener('onPlaybackState', next => {
+      console.log('[usePlaybackState] event:', next.status, 'isPlaying:', next.isPlaying, 'time:', next.currentTime.toFixed(1));
       latestStateRef.current = next;
       setState(next);
     });
