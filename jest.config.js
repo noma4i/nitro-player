93b6b936289786351dcc296dd1a13afc090140f2
Alexander Tsirel <noma4i@gmail.com>
2026-03-22 19:07:25 +1100

    Migrate tests to @testing-library/react

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src/__tests__'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
+  setupFiles: ['<rootDir>/jest.setup.js']
 };
--- src/__tests__/useNitroPlayer.test.tsx
+++ src/__tests__/useNitroPlayer.test.tsx
@@ -1,49 +1,44 @@
-import * as React from 'react';
-import * as TestRenderer from 'react-test-renderer';
-
-const { act } = TestRenderer;
+import { renderHook } from '@testing-library/react';
 
 jest.mock('react-native', () => ({
   Image: {
-    resolveAssetSource: jest.fn(),
+    resolveAssetSource: jest.fn()
   },
   Platform: {
-    select: jest.fn((config: Record<string, string>) => config.ios),
-  },
+    select: jest.fn((config: Record<string, string>) => config.ios)
+  }
 }));
 
 jest.mock('react-native-nitro-modules', () => ({
   NitroModules: {
     createHybridObject: jest.fn(() => ({
       fromUri: jest.fn(),
-      fromNitroPlayerConfig: jest.fn(),
-    })),
-  },
+      fromNitroPlayerConfig: jest.fn()
+    }))
+  }
 }));
 
-const addEventListener = jest.fn(
-  (_event: string, callback: () => void) => ({
-    remove: jest.fn(() => callback),
-  })
-);
+const addEventListener = jest.fn((_event: string, callback: () => void) => ({
+  remove: jest.fn(() => callback)
+}));
 
 const destroy = jest.fn();
 
 const playerInstance = {
   source: {
     config: {
-      initializeOnCreation: false,
-    },
+      initializeOnCreation: false
+    }
   },
   playbackState: {
-    status: 'idle',
+    status: 'idle'
   },
   addEventListener,
-  __destroy: destroy,
+  __destroy: destroy
 };
 
 jest.mock('../core/hooks/useManagedInstance', () => ({
-  useManagedInstance: jest.fn(() => playerInstance),
+  useManagedInstance: jest.fn(() => playerInstance)
 }));
 
 describe('useNitroPlayer', () => {
@@ -57,25 +52,11 @@ describe('useNitroPlayer', () => {
     const secondSetup = jest.fn();
     const { useNitroPlayer } = require('../core/hooks/useNitroPlayer');
 
-    function TestComponent({
-      setup,
-    }: {
-      setup: (player: typeof playerInstance) => void;
-    }) {
-      useNitroPlayer({ uri: 'https://cdn.example.com/video.mp4' }, setup);
-      return null;
-    }
-
-    let renderer: TestRenderer.ReactTestRenderer;
-    act(() => {
-      renderer = TestRenderer.create(<TestComponent setup={firstSetup} />);
-    });
+    const { rerender } = renderHook(({ setup }) => useNitroPlayer({ uri: 'https://cdn.example.com/video.mp4' }, setup), { initialProps: { setup: firstSetup } });
 
     expect(firstSetup).toHaveBeenCalledWith(playerInstance);
 
-    act(() => {
-      renderer!.update(<TestComponent setup={secondSetup} />);
-    });
+    rerender({ setup: secondSetup });
 
     expect(secondSetup).toHaveBeenCalledWith(playerInstance);
     expect(firstSetup).toHaveBeenCalledTimes(1);
